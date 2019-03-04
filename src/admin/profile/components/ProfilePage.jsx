41741253acad132bca4fbdafece0d21231595599
Trinh <mrnhuttrinh@gmail.com>
2019-03-04 16:55:38 +0700

    Dashboard

--- src/admin/profile/components/ProfilePage.jsx
+++ src/admin/profile/components/ProfilePage.jsx
@@ -1,9 +1,154 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import InputLabel from '@material-ui/core/InputLabel';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-function ProfilePage() {
+import PermIdentity from '@material-ui/icons/PermIdentity';
+
+import GridContainer from '../../../common/material-ui/components/Grid/GridContainer';
+import GridItem from '../../../common/material-ui/components/Grid/GridItem';
+import Button from '../../../common/material-ui/components/CustomButtons/Button';
+import CustomInput from '../../../common/material-ui/components/CustomInput/CustomInput';
+import Clearfix from '../../../common/material-ui/components/Clearfix/Clearfix';
+import Card from '../../../common/material-ui/components/Card/Card';
+import CardBody from '../../../common/material-ui/components/Card/CardBody';
+import CardHeader from '../../../common/material-ui/components/Card/CardHeader';
+import CardIcon from '../../../common/material-ui/components/Card/CardIcon';
+
+import { withMainLayout } from '../../hoc';
+
+import styles from './styles';
+
+function ProfilePage(props) {
+  const { classes } = props;
   return (
-    <p>ProfilePage</p>
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="rose" icon>
+              <CardIcon color="rose">
+                <PermIdentity />
+              </CardIcon>
+              <h4 className={classes.cardIconTitle}>
+                Edit Profile -
+                <small>
+                  Complete your profile
+                </small>
+              </h4>
+            </CardHeader>
+            <CardBody>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={5}>
+                  <CustomInput
+                    labelText="Company (disabled)"
+                    id="company-disabled"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                    inputProps={{
+                      disabled: true,
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={3}>
+                  <CustomInput
+                    labelText="Username"
+                    id="username"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={4}>
+                  <CustomInput
+                    labelText="Email address"
+                    id="email-address"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CustomInput
+                    labelText="First Name"
+                    id="first-name"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CustomInput
+                    labelText="Last Name"
+                    id="last-name"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={4}>
+                  <CustomInput
+                    labelText="City"
+                    id="city"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={4}>
+                  <CustomInput
+                    labelText="Country"
+                    id="country"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={4}>
+                  <CustomInput
+                    labelText="Postal Code"
+                    id="postal-code"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={12}>
+                  <InputLabel style={{ color: '#AAAAAA' }}>About me</InputLabel>
+                  <CustomInput
+                    labelText="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+                    id="about-me"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                    inputProps={{
+                      multiline: true,
+                      rows: 5,
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+              <Button color="rose" className={classes.updateProfileButton}>
+                Update Profile
+              </Button>
+              <Clearfix />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
   );
 }
 
-export default ProfilePage;
+ProfilePage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withMainLayout(withStyles(styles)(ProfilePage));
