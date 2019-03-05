308fb7ae0272e0ba5b6bd719e0f5e0f412f14eb4
Lam <hqlam.bt@gmail.com>
2019-03-05 12:14:53 +0700

    Add redux, redux-form, redux-dev-tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 // @flow
 import React, { Suspense } from 'react';
+import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
 import { hot } from 'react-hot-loader/root';
 import {
   Router, Route, Switch, Redirect,
 } from 'react-router-dom';
 import routes from './routes';
+import store from './store';
 
 export const history = createBrowserHistory();
 
 function App() {
   return (
-    <Router history={history} key={Math.random()}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          {routes.map(r => (
-            <Route
-              key={r.name}
-              path={r.path}
-              render={() => <r.component />}
-              exact
-            />
-          ))}
-          <Route path="/" render={() => <Redirect to="/admin/dashboard" />} exact />
-          {/* <Route component={NoMatch} /> */}
-        </Switch>
-      </Suspense>
-    </Router>
+    <Provider store={store}>
+      <Router history={history} key={Math.random()}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            {routes.map(r => (
+              <Route
+                key={r.name}
+                path={r.path}
+                render={() => <r.component />}
+                exact
+              />
+            ))}
+            <Route path="/" render={() => <Redirect to="/admin/dashboard" />} exact />
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
+        </Suspense>
+      </Router>
+    </Provider>
   );
 }
 
